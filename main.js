9ad3f200aba04eefd134e61ad0102d24bc04f7fa
Maxim Podgore <maximjulianpodgore@Gmail.com>
2024-07-31 17:34:45 -0700

    Mapview with double click pin placement works

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-/**
-import "./index.css";
-import { defineCustomElements as defineMapElements } from "@arcgis/map-components/dist/loader";
+require(["esri/Map", "esri/views/MapView", "esri/Graphic"],
+  (Map, MapView, Graphic) => {
+    // Create the Map
+    const map = new Map({
+      basemap: "topo-vector"
+      
+    });
+    console.log("Map Created");
 
-defineMapElements(window, {
-  resourcesUrl: "https://js.arcgis.com/map-components/4.30/assets",
-});
+    const view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [-117.2344, 32.8812], //long, lat
+      zoom: 12
+    });
+    console.log("View Created");
+    
 
-/**
- * Use `document.querySelector()` to get a reference to the `arcgis-map` component.
- * Add an event listener for the `arcgis-map` component's `viewReadyChange` event.
+    view.on("double-click", function(event){
+      createGraphic(event.mapPoint.latitude, event.mapPoint.longitude);
+      console.log("click");
+      console.log("Lat: " + event.mapPoint.latitude + ", Long: " + event.mapPoint.longitude);
+    });
 
-document
-  .querySelector("arcgis-map")
-  .addEventListener("arcgisViewReadyChange", (event) => {
-    /**
-     * Get a reference to the `WebMap`
-     * from the `event.detail` object.
-     */
-    //const { map } = event.detail;
-    // Add more functionality here.
-  //});
+    function createGraphic(lat, long){
+      // First create a point geometry 
+      var point = {
+        type: "point", // autocasts as new Point()
+        longitude: long,
+        latitude: lat
+      };
+    
+      // Create a symbol for drawing the point
+      var markerSymbol = {
+        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+        color: [226, 119, 40]
+      };
+    
+      // Create a graphic and add the geometry and symbol to it
+      var pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol
+      });
+    
+      // Add the graphics to the view's graphics layer
+      view.graphics.add(pointGraphic);
+    }
+  });
+
